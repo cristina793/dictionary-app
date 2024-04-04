71ab8842670611c84b73cae4a92af907bf1cede1
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-04 16:13:31 +0100

    added the api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,28 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
     let [keyWord, setKeyWord]=useState("");
 
-function handleKeywordChange(event){
-  
-    setKeyWord(event.target.value)
-}
-
+    function handleResponse(response){
+        console.log(response.data[0]);
+    }
 
     function search(event){
         event.preventDefault();
         alert(`Searching for ${keyWord}`)
-      
-    }
+  
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);    
+    } 
+
+     function handleKeywordChange(event){
+  
+    setKeyWord(event.target.value)
+     }
+
+
     return(
         <div className="Dictionary">
             
