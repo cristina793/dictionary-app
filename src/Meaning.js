ec9272ff7a595f0abb994b35fd797d226d5503dc
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-05 10:38:33 +0100

    added the Phonetic component

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,9 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
     return(<div className="Meaning">
-   
     <h3>{props.meaning.partOfSpeech}</h3>
     {props.meaning.definitions.map(function(definition,index){
         return(
---
+++ src/Phonetic.js
@@ -0,0 +1,8 @@
+import React from"react";
+
+export default function Phonetic(props){
+
+return (<div className="Phonetic">
+{props.phonetic.text} </div>);
+
+}
