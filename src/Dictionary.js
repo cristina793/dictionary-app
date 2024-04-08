7a9ce77789cfa9b0d56777fcdda11e96b3f5d9b1
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-08 16:20:28 +0100

    added the photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,18 +2,31 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from"./Results.js";
+import Photos from "./Photos";
 
 export default function Dictionary(props){
     let [loaded, setLoaded]=useState(false);    
     let [results, setResults]=useState(null);
     let [keyWord, setKeyWord]=useState(props.defaultKeyword);
+    let[photos, setPhotos]=useState(null);
 
-    function handleResponse(response){
+
+    function handleDictionaryResponse(response){
         setResults(response.data[0]);
     }
-function search(keyword){
+    function handlePexelsResponse(response){
+        console.log(response);
+        setPhotos(response.data.photos);
+    
+    }
+function search(){
    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
-    axios.get(apiUrl).then(handleResponse); 
+   axios.get(apiUrl).then(handleDictionaryResponse);
+
+let pexelsApiKey="GUBAAD4W0deohCAYsaXJeb2KiU74ZSkWpwjng9vCnwh0hOj1tA4FTVaf";
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=9`;
+let headers = { Authorization: `${pexelsApiKey}` };
+axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 
 }
     function handleSubmit(event){
@@ -22,29 +35,33 @@ function search(keyword){
     } 
 
      function handleKeywordChange(event){
-    setKeyWord(event.target.value)
+              setKeyWord(event.target.value);
      }
-     function load()
-     {setLoaded(true);
-    search();}
-if (loaded){
-
-    return(
-        <div className="Dictionary">
-            <section>
-                <h1>What word do you want to look up?</h1>
-        <form onSubmit={handleSubmit}>
-            <input
-            type="search"
-            onChange={handleKeywordChange}
-            className="form-floating"
-            defaultValue={props.defaultKeyword}/>
-           </form>
-           <div className="hint">
-            suggested words: sunset, yoga, forest...
-           </div>
-        </section>
-           < Results results={results} />
-           </div>
+            function load()
+            {setLoaded(true);
+                 search();
+            }
+         if (loaded){
+             return(
+                <div className="Dictionary">
+                   <section>
+                      <h1>What word do you want to look up?</h1>
+                       <form onSubmit={handleSubmit}>
+                           <input
+                             type="search"
+                             onChange={handleKeywordChange}
+                             className="form-floating"
+                             defaultValue={props.defaultKeyword}
+                             />
+                        </form>
+                        <div className="hint">
+                         suggested words: sunset, yoga, forest...
+                        </div>
+                   </section>
+                < Results results={results} />
+                < Photos photos={photos} />
+                </div>
        )
-}else{load(); return "Loading";}}
+}else{ 
+    load(); 
+    return "Loading";}}
